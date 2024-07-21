2aa00d906d37e294e85776f533c7d8189f626e0f
Delicia Fedele Boria <153142111+defedeleboria@users.noreply.github.com>
2024-07-20 22:20:24 -0300

    __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,9 @@
+# Importa funciones y clases específicas de los submódulos
+from .train_model import train_final_model, NutritionalRecommendationModel
 
+# Define qué funciones y clases estarán disponibles al importar el paquete
+__all__ = [
+    'train_final_model',
+    'NutritionalRecommendationModel',
+    # Se debería agregar cualquier otra función o clase que se quiera exportar
+]
