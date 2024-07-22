31ee498d6fdffcf1337892aec52ac0c8a0348409
Delicia Fedele Boria <153142111+defedeleboria@users.noreply.github.com>
2024-07-22 16:51:25 -0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+# Agregar el directorio principal del proyecto a sys.path
+current_path = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_path)
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
 import streamlit as st
 from app.main import app_logic
 
@@ -8,5 +17,5 @@ if __name__ == "__main__":
     main()
 
 # Añadir opción para guardar o imprimir la recomendación
-st.button("Save as PDF")
-st.button("Print")
+st.button("Exportar a PDF")
+st.button("Imprimir")
