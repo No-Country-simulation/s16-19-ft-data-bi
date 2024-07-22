352985ed08c1e923e8be86a35fc1f6269ce754b9
Delicia Fedele Boria <153142111+defedeleboria@users.noreply.github.com>
2024-07-22 11:47:28 -0300

    model.py

--- model/model.py
+++ model/model.py
@@ -1,1 +1,13 @@
+import torch.nn as nn
+import torch
 
+class NutritionalModel(nn.Module):
+    def __init__(self):
+        super(NutritionalModel, self).__init__()
+        self.fc1 = nn.Linear(10, 50)
+        self.fc2 = nn.Linear(50, 1)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
