226e90c5edfa4f8ba7a0c1e99b396b03c760b4f9
Delicia Fedele Boria <153142111+defedeleboria@users.noreply.github.com>
2024-07-22 11:47:53 -0300

    dataset.py

--- model/dataset.py
+++ model/dataset.py
@@ -1,1 +1,11 @@
+from torch.utils.data import Dataset
 
+class FoodDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
