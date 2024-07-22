7ce03d9aee632668d86d7cf80ce4cfd5c3666b8b
Delicia Fedele Boria <153142111+defedeleboria@users.noreply.github.com>
2024-07-22 11:42:46 -0300

    main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
+import streamlit as st
+from app.forms import user_form
+from app.recommendations import generate_plan
 
+def main():
+    st.title("Sistema de Recomendación Nutricional para Personas con Diabetes")
+    user_data = user_form()
+    
+    if st.button("Generar Plan Nutricional"):
+        plan = generate_plan(user_data)
+        st.write(plan)
+
+if __name__ == "__main__":
+    main()
