1b3ba35deef2cb8b21e860ccf8e94f095e6686f2
Delicia Fedele Boria <153142111+defedeleboria@users.noreply.github.com>
2024-07-23 17:57:50 -0300

    forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import time
 
 def user_form():
     edad = st.number_input("Edad", min_value=0, max_value=120, step=1)
@@ -6,8 +7,11 @@ def user_form():
     peso = st.number_input("Peso (kg)", min_value=0.0, step=0.1)
     altura = st.number_input("Altura (cm)", min_value=0.0, step=0.1)
     pais = st.text_input("País")
+    estacion = st.selectbox("Estación del Año?", ["Verano", "Otoño", "Primavera", "Invierno"])
     nivel_actividad = st.selectbox("Nivel de Actividad Física", ["Bajo", "Moderado", "Intenso"])
     horas_sueno = st.number_input("Horas de Sueño Promedio por Día", min_value=0, max_value=24, step=1)
+    inicio_act = st.time_input("Hora de Despertarse", time(6, 0))  # Valor predeterminado 6:00 AM
+    fin_act = st.time_input("Hora de Dormirse", time(23, 0))  # Valor predeterminado 11:00 PM
     patologia = st.multiselect("Patología Subyacente", ["Enfermedad Cardiovascular", "Hipertensión", "Dislipemia", "Gastroparesia", "Hígado Graso"])
     tipo_diabetes = st.selectbox("Tipo de Diabetes", ["Tipo I", "Tipo II"])
     preferencia_dietetica = st.text_input("Preferencia Dietética")
@@ -19,8 +23,11 @@ def user_form():
         "peso": peso,
         "altura": altura,
         "pais": pais,
+        "estacion": estacion,
         "nivel_actividad": nivel_actividad,
         "horas_sueno": horas_sueno,
+        "inicio_act": inicio_act,
+        "fin_act": fin_act,
         "patologia": patologia,
         "tipo_diabetes": tipo_diabetes,
         "preferencia_dietetica": preferencia_dietetica,
